f8c643a3212efea916d74342d66347350807cc69
marcosccm <marcosmatos@cultureamp.com>
2017-12-02 22:08:30 +1100

    Part 2 of Day 1 challenge
    Just realized it had 2 parts

--- 1-inverse-captcha/InverseCaptcha.elm
+++ 1-inverse-captcha/InverseCaptcha.elm
@@ -1,22 +1,55 @@
 module InverseCaptcha exposing (..)
 
+import Array
+
 
 calculate : String -> Int
 calculate entry =
     let
-        mapConsecutiveNumber a b =
-            if a == b then
-                Just a
-            else
-                Nothing
-
         chars =
             String.split "" entry
     in
-        List.map2 mapConsecutiveNumber chars (shiftFirstToLast chars)
+        List.map2 keepIfMatch chars (shiftFirstToLast chars)
             |> List.filterMap identity
             |> List.map (String.toInt >> Result.toMaybe >> Maybe.withDefault 0)
             |> List.sum
+
+
+calculate2 : String -> Int
+calculate2 entry =
+    let
+        chars =
+            String.split "" entry
+
+        halfPoint =
+            (List.length chars |> toFloat) / 2 |> round
+
+        fullLength =
+            List.length chars
+
+        entryArray =
+            Array.fromList chars
+
+        halfwayList =
+            List.indexedMap
+                (\pos _ ->
+                    Array.get ((pos + halfPoint) % fullLength) entryArray
+                )
+                chars
+                |> List.filterMap identity
+    in
+        List.map2 keepIfMatch chars halfwayList
+            |> List.filterMap identity
+            |> List.map (String.toInt >> Result.toMaybe >> Maybe.withDefault 0)
+            |> List.sum
+
+
+keepIfMatch : a -> a -> Maybe a
+keepIfMatch a b =
+    if a == b then
+        Just a
+    else
+        Nothing
 
 
 shiftFirstToLast : List a -> List a
