0a7b53540d27534c9e1a2433fd15a320362b07c7
marcosccm <marcosmatos@cultureamp.com>
2017-12-02 22:57:50 +1100

    Day 2 challenge part 2

--- 2-checksum/Checksum.elm
+++ 2-checksum/Checksum.elm
@@ -1,4 +1,4 @@
-module Checksum exposing (calculate)
+module Checksum exposing (calculate, calculate2)
 
 
 calculate : List (List Int) -> Int
@@ -9,6 +9,33 @@ calculate entry =
                 (List.maximum row)
                 (List.minimum row)
                 |> Maybe.withDefault 0
+    in
+        List.map rowChecksum entry
+            |> List.sum
+
+
+calculate2 : List (List Int) -> Int
+calculate2 entry =
+    let
+        exactDivision a b =
+            if rem a b == 0 then
+                Just (a // b)
+            else if rem b a == 0 then
+                Just (b // a)
+            else
+                Nothing
+
+        findExactDivision : List Int -> Int -> Int -> Int
+        findExactDivision row index elem =
+            List.map (exactDivision elem) (List.drop (index + 1) row)
+                |> List.filterMap identity
+                |> List.head
+                |> Maybe.withDefault 0
+
+        rowChecksum : List Int -> Int
+        rowChecksum row =
+            List.indexedMap (findExactDivision row) row
+                |> List.sum
     in
         List.map rowChecksum entry
             |> List.sum
--- 2-checksum/tests/ChecksumTest.elm
+++ 2-checksum/tests/ChecksumTest.elm
@@ -5,9 +5,9 @@ import Test exposing (Test, describe, test)
 import Expect
 
 
-suite : Test
-suite =
-    describe "checksum"
+checksum1 : Test
+checksum1 =
+    describe "checksum 1"
         [ test "sum of the difference between the largest and the smallest element for each column" <|
             \() ->
                 Checksum.calculate
@@ -19,4 +19,18 @@ suite =
             \() ->
                 Checksum.calculate []
                     |> Expect.equal 0
+        ]
+
+
+checksum2 : Test
+checksum2 =
+    describe "checksum 2"
+        [ test "finds a pair of numbers on each row that divides exactly and sum the results of the division" <|
+            \() ->
+                Checksum.calculate2
+                    [ [ 5, 9, 2, 8 ]
+                    , [ 9, 4, 7, 3 ]
+                    , [ 3, 8, 6, 5 ]
+                    ]
+                    |> Expect.equal 9
         ]
